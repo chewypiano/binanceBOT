7d27a1cb523ee8807c0e89f17a1c489341777654
chewypiano <gimikamil@gmail.com>
2021-05-27 11:02:28 +0800

    Add RiskWatcher class

--- src/main/java/Analytics/RiskWatcher.java
+++ src/main/java/Analytics/RiskWatcher.java
@@ -1,4 +1,35 @@
 package Analytics;
 
-public class RiskWatcher {
+import Messaging.EventListener;
+import Scheduling.ScheduleEvent;
+import Source.OrderBook;
+
+public class RiskWatcher implements EventListener {
+
+    private double imbalance;
+
+    /**
+     * Creates a RiskWatcher.
+     */
+    public RiskWatcher() {
+        this.imbalance = 0;
+    }
+
+    @Override
+    public void handleEvent(OrderBook orderBook) throws InterruptedException {
+        this.imbalance = OrderBookImbalance.computeOrderImbalance(orderBook);
+    }
+
+    @Override
+    public void handleEvent(ScheduleEvent timer) throws InterruptedException {
+        String tag = timer.getTag();
+        if (!tag.equals("orderImbalance")) {
+            return;
+        }
+        if (imbalance < 0) {
+            System.out.println("Ask surplus, Imbalance : " + imbalance);
+        } else {
+            System.out.println("Bid surplus, Imbalance : " + imbalance);
+        }
+    }
 }
